4e51ed69293fb3418d6ac638fa130071fe7ec9e5
Vikas <vikas.karthik06@gmail.com>
2026-03-08 12:18:51 +0530

    First Commit

---
+++ app/api/main.py
@@ -0,0 +1,88 @@
+from fastapi import FastAPI, Query, Path, Body, Request
+import time
+from pydantic import BaseModel, Field
+from typing import Literal, Annotated
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+class FilterParams(BaseModel):
+    limit: int = Field(100, gt=0, le=100)
+    offset: int = Field(0, ge=0)
+    order_by: Literal["created_at", "updated_at"] = "created_at"
+    tags: list[str] = []
+
+
+@app.get("/items/")
+async def read_items(filter_query: Annotated[FilterParams, Query()]):
+    return filter_query
+
+@app.get("/events/")
+async def trigger_event(event_name:str=None):
+    return {"Current event" : event_name}
+
+@app.get("/fakeDB/")
+async def read_item(item: Item):
+    return item
+
+@app.put("/nitems/{item_id}")
+async def update_item(
+    *,
+    item_id: int,
+    item: Annotated[
+        Item,
+        Body(
+            openapi_examples={
+                "normal": {
+                    "summary": "A normal example",
+                    "description": "A **normal** item works correctly.",
+                    "value": {
+                        "name": "Foo",
+                        "description": "A very nice Item",
+                        "price": 35.4,
+                        "tax": 3.2,
+                    },
+                },
+                "converted": {
+                    "summary": "An example with converted data",
+                    "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                    "value": {
+                        "name": "Bar",
+                        "price": "35.4",
+                    },
+                },
+                "invalid": {
+                    "summary": "Invalid data is rejected with an error",
+                    "value": {
+                        "name": "Baz",
+                        "price": "thirty five point four",
+                    },
+                },
+            },
+        ),
+    ],
+):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from algostore!")
+
+
+if __name__ == "__main__":
+    main()
